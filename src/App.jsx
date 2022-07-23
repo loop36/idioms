a45601d5d7b2e3e043dcd086055f525f570d06d5
unknown <anandvb2013@gmail.com>
2022-07-23 22:19:59 +0530

    Production

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,36 @@ import { useState } from "react";
 
 import Card from "./components/Card";
 import "./App.css";
-//import data from "./assets/idioms.json"
+import axios from "axios";
 import { useEffect } from "react";
 function App() {
   const [index, setIndex] = useState(0);
-  
+  const [idioms,setIdioms]= useState([]);
   useEffect(()=>{
    randomIdiom();
+   axios.get("idioms.json").then((idiomsData)=>{
+      setIdioms(idiomsData.data)
+   })
   },[])
   const randomIdiom= ()=> {
-    setIndex(Math.floor(Math.random() * data.length));
-    console.log("executed")
+    if(idioms.length!=0){
+    setIndex(Math.floor(Math.random() * idioms.length));
+    
+    }
+    else{
+      setIndex(Math.floor(Math.random() * 1200));
+    }
   }
   return (
     <div className="App">
+      {idioms.length==0?null:<>
       <h2 style={{ color: "rgba(230,230,230,1)"}}>Idioms for Day</h2>
       <Card click={randomIdiom}>
-        <h1 style={{ color: "rgba(255,255,210,1)" } }>{data[index].title}</h1>
-        <p style={{ color: "rgba(255,255,210,1)" }}>{data[index].desc}</p>
+        <h1 style={{ color: "rgba(255,255,210,1)" } }>{idioms[index].title}</h1>
+        <p style={{ color: "rgba(255,255,210,1)" }}>{idioms[index].desc}</p>
+        
       </Card>
+      </>}
       
     </div>
   );

069dde3f94039b9f0e476968a044704f78604cf3
loop36 <loop36@users.noreply.github.com>
2023-06-10 17:09:07 +0000

    deploy: 4be2b29fc298e37ebc234c76448ee02d88179bfc

--- src/components/Card.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react'
-import "./card.css"
-const Card = (props) => {
-  return(<div className="container" onClick={()=>{props.click()}}>
-   {props.children}
-  </div>);
-};
-export default Card;

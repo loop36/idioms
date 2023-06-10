7927f7d6af6ccc065c49d1058c4694e28d02b026
Anand Balagopalan <anand.b@qburst.com>
2023-06-10 23:27:31 +0530

    fix again

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  base: "/idioms/",
-  plugins: [reactRefresh()],
-};
+export default defineConfig({
+  base:'/idioms/',
+  plugins: [react()]
+})
